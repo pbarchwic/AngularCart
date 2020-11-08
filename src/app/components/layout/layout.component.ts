6318ed7f7e528a4ce5dcb2627e15e8d64e8693c7
Patryk Barchwic <patryk.barchwic@predica.pl>
2020-11-08 14:45:53 +0100

    Refactor

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -1,9 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
-import { ProductDetails, ProductsResponse, Response } from '../../models';
-import { ProductRepository } from 'src/app/repository';
 
 @Component({
   selector: 'app-layout',
@@ -11,22 +6,7 @@ import { ProductRepository } from 'src/app/repository';
   styleUrls: ['./layout.component.scss'],
 })
 export class LayoutComponent implements OnInit {
-  public products: ProductDetails[];
-  constructor(
-    private readonly http: HttpClient,
-    private readonly repository: ProductRepository
-  ) {}
+  constructor() {}
 
-  ngOnInit(): void {
-    this.getProducts();
-  }
-
-  public getProducts(): void {
-    this.repository.getProductDetails().subscribe(
-      (response: ProductsResponse) => {
-        this.products = response.products;
-      },
-      () => {}
-    );
-  }
+  ngOnInit(): void {}
 }
--- src/app/components/product-card/product-card.component.ts
+++ src/app/components/product-card/product-card.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { ProductDetails } from 'src/app/models';
+import { Component, OnInit } from '@angular/core';
+import { ProductDetails, ProductsResponse } from 'src/app/models';
+import { ProductRepository } from 'src/app/repository';
 import { CartService } from 'src/app/services';
 
 @Component({
@@ -8,13 +9,27 @@ import { CartService } from 'src/app/services';
   styleUrls: ['./product-card.component.scss'],
 })
 export class ProductCardComponent implements OnInit {
-  @Input() product: ProductDetails;
-  constructor(private readonly cartService: CartService) {}
+  public products: ProductDetails[];
+  constructor(
+    private readonly cartService: CartService,
+    private readonly repository: ProductRepository
+  ) {}
 
-  ngOnInit() {}
+  ngOnInit(): void {
+    this.getProducts();
+  }
 
   public addToCart(product: ProductDetails): void {
     product.inCart = true;
     this.cartService.addToCart(product);
+  }
+
+  public getProducts(): void {
+    this.repository.getProductDetails().subscribe(
+      (response: ProductsResponse) => {
+        this.products = response.products;
+      },
+      () => {}
+    );
   }
 }
